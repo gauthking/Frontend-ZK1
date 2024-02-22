d647d7f1f48c40ef41d469cb03087228c725a441
Gautham Krishna <90638995+gauthking@users.noreply.github.com>
2024-02-22 22:58:34 +0530

    added backend link to env

--- app/axios.ts
+++ app/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "https://zkw-backend-api.onrender.com", //https://zkapitypesc.onrender.com
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_API, //https://zkapitypesc.onrender.com
 })
 
 export default instance
