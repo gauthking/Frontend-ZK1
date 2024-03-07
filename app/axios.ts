8924a510b408f7d14f35ab72713f1563aaa127fd
Gautham Krishna <90638995+gauthking@users.noreply.github.com>
2024-03-07 14:09:33 +0530

    Update axios.ts

--- app/axios.ts
+++ app/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "http://ec2-3-218-238-103.compute-1.amazonaws.com", //https://zkapitypesc.onrender.com
+    baseURL: "https://backend.l2-zk-api.com", //https://zkapitypesc.onrender.com
 })
 
 export default instance
